0d1c1723b47a5e1c1272d8aefbf28a469ba13139
HrHossain <mofazzelhossain185@gmail.com>
2025-11-15 21:07:39 +0600

    done custom hook for fetching data useWeather

---
+++ src/hooks/useWeather.js
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from 'react'
+
+const useWeather = () => {
+  const [loading,setLoading] = useState({state:false,message:""})
+  const [error,setError] = useState(null)
+  const [weatherData,setWeatherData] = useState({
+    location:"",
+    climate:"",
+    temperature:"",
+    maxTemperature:"",
+    minTemperature:"",
+    humidity:"",
+    cloudPercentage:"",
+    wind:"",
+    time:"",
+    longitude:"",
+    latitude:""
+  })
+
+  async function fetchWeather ( lat , lon ){
+    try{
+        setLoading({
+            ...loading,
+            state:true,
+            message:"Weather Data Collecting from API..."
+        })
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${import.meta.env.VITE_WEATHER_API_KEY}&units=metric`)
+        if(!response.ok){
+            const errorMessage = `Data Fetching Is Failed ${response.status}`
+            throw new Error(errorMessage)
+        }
+        const data = await response.json()
+        const updateWeatherData = {
+            location:data?.name,
+            climate:data?.weather[0]?.main,
+            temperature:data?.main?.temp,
+            maxTemperature:data?.main?.temp_max,
+            minTemperature:data?.main?.temp_min,
+            humidity:data?.main?.humidity,
+            cloudPercentage:data?.clouds.all,
+            wind:data?.wind?.speed,
+            time:data?.dt,
+            longitude:data?.coord?.lon,
+            latitude:data?.coord?.lat
+
+        }
+        setWeatherData(updateWeatherData)
+
+
+    }catch(err){
+        setError(err)
+
+    }finally{
+
+        setLoading({
+            ...loading,
+            state:false,
+            message:""
+        })
+
+    }
+  }
+
+  useEffect(()=>{
+    let key = false
+
+    setLoading({
+        ...loading,
+        state:true,
+        message:"Data Fetching..."
+    })
+    if(!key){
+        navigator.geolocation.getCurrentPosition(({coords})=>{
+            fetchWeather(coords.latitude , coords.longitude)
+        })
+    }
+
+    return ()=>{
+        key = true
+    }
+  },[])
+
+  return {
+    loading,
+    error,
+    weatherData
+  }
+}
+
+export default useWeather
